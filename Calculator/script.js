f2a946a03d1e9ea3a1cd79a39f3facc7e00b3952
X <Nhlapho7@gmail.com>
2020-10-01 01:18:01 +0200

    Initial comment

---
+++ Calculator/script.js
@@ -0,0 +1,14 @@
+class Calculator {
+    constructor(previousOperandButton, currentOperandButton) {
+        this.previousOperandButton = previousOperandButton
+        this.currentOperandButton = currentOperandButton
+    }
+}
+
+const numberButton = document.querySelectorAll("[data-number]") //gets all the number operands
+const operationButton = document.querySelectorAll("[data-operation]") //gets all the operation operands
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandButton = document.querySelector('[data-previous-operand]')
+const currentOperandButton = document.querySelector('[data-current-operand]')
